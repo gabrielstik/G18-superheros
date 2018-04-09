65876b6d5be2a82c74efc1ba330d2e6ee2eaa1de
Gabriel Stik <gabriel.stik@gmail.com>
2018-04-09 11:19:50 +0200

    :wrench: Remote Db connected

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?
-session_start();
-setlocale(LC_ALL, 'fr_FR');
+// session_start();
+// setlocale(LC_ALL, 'fr_FR');
 require './config.php';
 
-$page = isset($_GET['q']) ? $_GET['q'] : 'home';
+// $page = isset($_GET['q']) ? $_GET['q'] : 'home';
