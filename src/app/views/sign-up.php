8fef702ad41e92af8972ec5561d47e1ecdf79809
Gabriel Stik <gabriel.stik@gmail.com>
2018-04-10 13:18:23 +0200

    :sparkles: Made sign-up prestyle

--- src/app/views/sign-up.php
+++ src/app/views/sign-up.php
@@ -1,12 +1,25 @@
-<form id="sign-up" action="/sign-up" method="post">
-  <label for="sign-up--username">Nom d'utilisateur</label>
-  <input type="text" name="sign-up--username" id="sign-up--username">
-  <label for="sign-up--password">Mot de passe</label>
-  <input type="password" name="sign-up--password" id="sign-up--password">
-  <label for="sign-up--password-confirm">Confirmer le mot de passe</label>
-  <input type="password" name="sign-up--password-confirm" id="sign-up--password-confirm">
-  <label for="sign-up--alias">Pseudonyme</label>
-  <input type="text" name="sign-up--alias" id="sign-up--alias">
-  <input type="file" name="sign-up--picture" id="sign-up--picture" accept=".jpg, .jpeg, .png">
-  <input type="submit" name="sign-up" value="Créer un compte">
+<form class="form--sign-up" id="sign-up" action="/sign-up" method="post">
+  <h2 class="form--title">Créer un compte</h2>
+  <div class="form--item">
+    <label class="form-text" for="sign-up--username">Nom d'utilisateur</label>
+    <input class="form-text" ype="text" name="sign-up--username" id="sign-up--username">
+  </div>
+  <div class="form--item">
+    <label class="form-text" for="sign-up--password">Mot de passe</label>
+    <input class="form-text" type="password" name="sign-up--password" id="sign-up--password">
+  </div>
+  <div class="form--item">
+    <label class="form-text" for="sign-up--password-confirm">Confirmer le mot de passe</label>
+    <input class="form-text" type="password" name="sign-up--password-confirm" id="sign-up--password-confirm">
+  </div>
+  <div class="form--item">
+    <label class="form-text" for="sign-up--alias">Pseudonyme</label>
+    <input class="form-text" type="text" name="sign-up--alias" id="sign-up--alias">
+  </div>
+  <div class="form--item">
+    <input type="file" name="sign-up--picture" id="sign-up--picture" accept=".jpg, .jpeg, .png">
+  </div>
+  <div class="form--item flex flex_around">
+    <input class="submit" type="submit" name="sign-up" value="Créer un compte">
+  </div>
 </form>
